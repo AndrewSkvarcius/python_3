72a9283d1d89805592acedc36c0b5a711dcb7574
[Andrew Skvarcius] <andrewskvarcius@gmail.com>
2023-03-23 14:38:20 -0400

    files added

---
+++ words.py
@@ -0,0 +1,23 @@
+def print_upper_words(words) :
+    '''Prints each word on seperate line uppercase'''
+    for word in words :
+        print(word.upper())
+
+def print_lower_words(words) :
+    '''Prints each word on seperate line lowercase'''
+    for word in words:
+        print(word.lower())
+
+def print_upper_words_t (words): 
+    "prints words with T"
+    for word in words :
+        if word.startswith("t") or word.startswith("T"):
+            print(word.upper())
+
+def print_upper_wordz(words, must_start_with):
+    '''Prints each word upper if it starts with'''
+    for word in words:
+        for letter in must_start_with:
+            if word.startswith(letter):
+                print(word.upper())
+                break
